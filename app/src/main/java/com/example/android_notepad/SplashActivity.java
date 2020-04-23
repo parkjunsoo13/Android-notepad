b747a77ba7001b9c2adf9d5733fc297c5fac4744
parkjunsoo <qkrwnstn13@naver.com>
2020-04-23 18:28:33 +0900

    notepad

---
+++ app/src/main/java/com/example/android_notepad/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.android_notepad;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        finish();
+    }
+}
